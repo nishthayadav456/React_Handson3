2f2abfcbfe7ff0d7ca4cfba9ef085eab295538de
nishthayadav456 <nishthayadav1122@gmail.com>
2023-07-21 12:00:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormClass from './Components/FormClass';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    
+   <FormClass/>
+    </>
+  );
+}
+
+export default App;
